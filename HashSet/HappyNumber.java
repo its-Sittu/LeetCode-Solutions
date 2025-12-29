c91a128dd214e82b1cc437505947a606b07150c7
Sittu Kumar Singh <sitturaj730@gmail.com>
2025-12-29 23:43:01 +0530

    Update HappyNumber.java

--- HashSet/HappyNumber.java
+++ HashSet/HappyNumber.java
@@ -1,1 +1,23 @@
+  class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> Set = new HashSet<>();
 
+        while (true) {
+
+            int sum = 0;
+           while(n!=0){
+            sum += Math.pow(n%10 ,2.0);
+            n = n / 10;
+           }
+
+            if (sum == 1)
+                return true;
+
+            n = sum;
+            if (Set.contains(n)) 
+                return false;
+                  Set.add(n); 
+        }
+    }
+}
+//   isse practice kro and dry run bhi khudse
