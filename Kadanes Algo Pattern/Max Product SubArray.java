e6af81d9bb57932bad91b4359861b091d3f94390
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-01-02 21:15:06 +0530

    152. Maximum Product Subarray

---
+++ Kadanes Algo Pattern/Max Product SubArray.java
@@ -0,0 +1,25 @@
+//Leet Code No 152 
+
+// isme na 4 variables hai 
+// jo operate krte hai to find max product Subarray
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int maxEnd = nums[0];
+        int minEnd = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            int v1 = nums[i];
+            int v2 = minEnd * nums[i];
+            int v3 = maxEnd * nums[i];
+            maxEnd = Math.max(v1, Math.max(v2, v3));
+            minEnd = Math.min(v1, Math.min(v2, v3));
+
+            ans = Math.max(ans, Math.max(maxEnd, minEnd));
+
+        }
+        return ans;
+    }
+}
