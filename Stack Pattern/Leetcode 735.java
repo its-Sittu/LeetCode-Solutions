609fd3dffafc656ce0634cf921fb662cc0b58c63
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-03-24 03:44:01 +0530

    Asteroid Collision // Optimise Code

--- Stack Pattern/Leetcode 735.java
+++ Stack Pattern/Leetcode 735.java
@@ -1,3 +1,36 @@
 // Stack 
 // Optimse Code
 // Medium Level
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        int n = asteroids.length;
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            if (asteroids[i] > 0) {
+                st.push(asteroids[i]);
+            } else {
+                while (!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(asteroids[i])) {
+                    st.pop();
+                }
+                if(!st.isEmpty() && st.peek() == Math.abs(asteroids[i])){
+                    st.pop();
+                    continue;
+                } else if(!st.isEmpty() && st.peek()>Math.abs(asteroids[i])){
+                    continue;
+                } else{
+                   if( st.isEmpty() || st.peek()<0){
+                    st.push(asteroids[i]);
+                }
+                }
+            }
+        }
+        int[] res = new int[st.size()];
+        for (int i = st.size() - 1; i >= 0; i--) {
+            res[i] = st.pop();
+        }
+
+        return res;
+    }
+}
