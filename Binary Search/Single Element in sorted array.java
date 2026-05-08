d8707d9548dce6704e2eb7a7d189f8d505466ac9
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-05-09 03:31:40 +0530

    Add brute force method to find single non-duplicate element
    Implement brute force solution to find single non-duplicate element in sorted array.

---
+++ Binary Search/Single Element in sorted array.java
@@ -0,0 +1,28 @@
+// brute code 
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+
+  
+
+        //outer loop
+        for (int i = 0; i < n; i++) {
+
+            
+          int count = 0;
+
+            for (int j = 0; j <n; j++) {
+
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
