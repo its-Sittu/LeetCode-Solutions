a0aeab722b031d5f391220d1ef6bde26e2c76df4
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-05-18 03:32:45 +0530

    Add binary search method for square root calculation
    Implemented binary search to find the square root of a number.

--- Binary Search/Sqrt.java
+++ Binary Search/Sqrt.java
@@ -19,3 +19,26 @@ class Solution {
 
 // optimise ~ by Using Binary Search
 
+class Solution {
+    public int mySqrt(int x) {
+        int low = 0;
+        int high = x;
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            long sqr = (long) mid * mid;
+
+            if (sqr == x) {
+                return mid;
+            } else if (sqr < x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
