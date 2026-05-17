7d065442c1f8d7e353390642d3bf08f270e6b634
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-05-18 02:12:36 +0530

    Leetcode 69

---
+++ Binary Search/Sqrt.java
@@ -0,0 +1,17 @@
+// brute force 
+
+class Solution {
+    public int mySqrt(int x) {
+
+        int ans = 0;
+        for (int i = 0; i <=x; i++) {
+
+            if ((long)i * i <= x) {
+                ans = i;
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+}
