4b06d6baeb9a915f1445f175b82b0f30595b9d98
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-05-19 05:21:27 +0530

    GFG  Ques
    Implement brute force method to find nth root of m.

---
+++ Binary Search/Find nth root of n.java
@@ -0,0 +1,18 @@
+// brute force 
+
+class Solution {
+    public int nthRoot(int n, int m) {
+        // code here
+        for(int i = 0; i <=m; i++){
+            
+            
+            long power = (long) Math.pow(i,n);
+            if(power == m){
+                return i;
+            } else if(power > m){
+                break;
+            }
+        }
+        return -1;
+    }
+}
