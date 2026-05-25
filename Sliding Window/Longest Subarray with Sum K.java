1e2cfafe7b1fd3e75e5a5d01d08b8b819648b3bf
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-05-26 04:43:36 +0530

    Update Longest Subarray with Sum K.java

--- Sliding Window/Longest Subarray with Sum K.java
+++ Sliding Window/Longest Subarray with Sum K.java
@@ -1,1 +1,23 @@
 //Brute Force
+// User function Template for Java
+
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        // code here
+        int maxlength = 0;
+        
+        for(int i =0; i<=arr.length-1; i++){
+            int sum = 0;
+            for(int j = i ; j<=arr.length-1; j++){
+                sum = sum + arr[j];
+                
+                if(sum == k){
+                    maxlength = Math.max(maxlength , j-i+1);
+                }
+            }
+        }
+        return maxlength;
+    }
+}
+
+// 
