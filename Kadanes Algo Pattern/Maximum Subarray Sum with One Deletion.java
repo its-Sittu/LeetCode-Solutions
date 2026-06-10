379ee8a4ab0faa97bf546fb9efd0a82d995ac348
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-06-10 21:48:39 +0530

    Fix loop syntax in Maximum Subarray Sum algorithm

--- Kadanes Algo Pattern/Maximum Subarray Sum with One Deletion.java
+++ Kadanes Algo Pattern/Maximum Subarray Sum with One Deletion.java
@@ -5,7 +5,7 @@ class Solution {
         int prevOnedel = 0;
         int res = arr[0];
 
-        for(int i =1; i<n; i++){
+        for(int i =1; i<n; i++){ 
             prevOnedel = Math.max(prevOnedel+ arr[i] ,prevNodel);
             prevNodel = Math.max(prevNodel+arr[i] , arr[i]);
             int current = Math.max(prevOnedel , prevNodel);
