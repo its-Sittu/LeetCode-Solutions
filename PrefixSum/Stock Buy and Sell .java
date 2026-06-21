b1b06d878384948f940406388f4714dcdf8120e5
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-06-21 23:28:19 +0530

    Leetcode 121

---
+++ PrefixSum/Stock Buy and Sell .java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+    int ans = 0;
+    int min_ele = prices[0];
+
+    for(int i = 0; i<prices.length; i++){
+
+      min_ele = Math.min(min_ele , prices[i]);
+      int profit = prices[i] - min_ele;
+
+      ans = Math.max(ans ,profit);
+    }
+    return ans;
+    }
+}
