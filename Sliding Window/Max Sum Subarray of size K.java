047416a0f1ee25392e360cc5b50669eda548195f
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-06-29 07:06:58 +0530

    Add brute force method for max subarray sum
    Implemented a brute force solution to find the maximum sum of a subarray of size K.

--- Sliding Window/Max Sum Subarray of size K.java
+++ Sliding Window/Max Sum Subarray of size K.java
@@ -1,1 +1,21 @@
 // Brute force
+
+class Solution {
+	public int maxSubarraySum(int[] arr, int k) {
+		// Code here
+		int n = arr.length;
+		
+		int Maxsum = Integer.MIN_VALUE;
+		
+		for (int i = 0; i <= n - k; i++) {
+			int sum = 0;
+			
+			for (int j = i; j<i + k; j++) {
+				
+				sum += arr[j];
+			}
+			Maxsum = Math.max(Maxsum, sum);
+		}
+		return Maxsum;
+	}
+}
