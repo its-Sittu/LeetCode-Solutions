582924d1ef20c2fcb9566e8c4ed18d36eadca266
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-06-30 23:54:46 +0530

    Add solution for first negative number in sliding window
    Implemented a solution to find the first negative number in every sliding window of size k.

--- Sliding Window/First Negative Number in Every Window of Size K.java
+++ Sliding Window/First Negative Number in Every Window of Size K.java
@@ -1,2 +1,29 @@
 // brute code 
 
+class Solution {
+    static List<Integer> firstNegInt(int arr[], int k) {
+        // code here
+        int n = arr.length;
+        
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i<n-k+1; i++ ){
+            
+            int ans = 0;
+            
+            for(int j = i; j<i+k; j++){
+                
+                if( arr[j] < 0){
+                    
+                    ans = arr[j];
+                    break;
+                    
+                } 
+            }
+             list.add(ans);
+            
+        }
+        return list;
+    }
+}
