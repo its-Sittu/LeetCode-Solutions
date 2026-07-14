76f88e063cb351cbb71829d77a567986e58eec19
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-14 08:06:18 +0530

    Add character removal method in Solution class
    Implemented a method to remove specified characters from a string.

--- Strings/remove character.java
+++ Strings/remove character.java
@@ -1,3 +1,28 @@
 // optimise code 
 
+class Solution {
+    static String removeChars(String str1, String str2) {
+        // code here
+        int n = str1.length();
+        int m = str2.length();
+        
+        int [] freq = new int[26];
 
+        
+        for(int i = 0; i<m; i++){
+            freq[str2.charAt(i)-'a'] = 1;
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        
+        for(int i = 0; i<n; i++){
+            char ch = str1.charAt(i);
+            
+           if( freq[ch - 'a'] == 0){
+             ans.append(ch);
+           }
+        }
+        
+        return ans.toString();
+    }
+}
