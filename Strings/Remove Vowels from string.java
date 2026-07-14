7bba25ff45d3a1a1daa1aa61345beb9f1d8eda40
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-14 06:55:30 +0530

    Add brute force method to remove vowels from string
    Implement brute force method to remove vowels from a string.

--- Strings/Remove Vowels from string.java
+++ Strings/Remove Vowels from string.java
@@ -1,1 +1,16 @@
 // Brute force approach
+
+String ans = " ";
+
+for(-------------){
+  char ch = s.charAt(i);
+  // mtlb uss string ke  andar ka elements
+
+  if(ch !="a" && -------------------){
+    // Similarly for all vowels 
+
+    ans = ans + ch;
+  }
+}
+return ans;
+}
