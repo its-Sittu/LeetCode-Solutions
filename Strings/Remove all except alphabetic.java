a16b5b15b4204fcd8efe2e2e34585c96c7d64c4c
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-15 07:38:18 +0530

    Optimize removeChars to retain only alphabetic characters
    Optimized the removeChars method to filter out non-alphabetic characters from the input string.

--- Strings/Remove all except alphabetic.java
+++ Strings/Remove all except alphabetic.java
@@ -1,1 +1,22 @@
+// optimise code 
 
+class Solution {
+    public String removeChars(String s) {
+        // code here
+        int n = s.length();
+        
+        StringBuilder ans = new StringBuilder();
+        
+        
+        for(int i =0; i<n ; i++){
+            char ch = s.charAt(i);
+            
+            if(ch >='a' && ch <='z' ){
+                ans.append(ch);
+            } else if( ch >='A' && ch <='Z'){
+                ans.append(ch);
+            }
+        }
+        return ans.toString();
+    }
+};
