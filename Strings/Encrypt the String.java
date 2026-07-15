656ab44512635fe9dbcb2a3a05e9aba90f6cb09b
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-15 09:14:42 +0530

    Optimize string encryption method
    Optimized the string encryption logic to count consecutive characters and reverse the result.

--- Strings/Encrypt the String.java
+++ Strings/Encrypt the String.java
@@ -1,1 +1,25 @@
 // optimise code 
+
+class Solution {
+    String encryptString(String s) {
+        // code here
+        int n = s.length();
+        int i = 0;
+        StringBuilder ans = new StringBuilder();
+        
+       while(i<n){
+          int count = 1;
+          char ch = s.charAt(i);
+          
+          while(i+1<n && s.charAt(i) == s.charAt(i+1)){
+              count++;
+              i++;
+          }
+          ans.append(ch);
+          ans.append(count);
+          i++;
+       }
+       ans.reverse();
+       return ans.toString();
+    }
+}
