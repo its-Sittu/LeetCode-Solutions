f23a121561407e2f157e0dafe97e39f64476514c
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-18 00:50:21 +0530

    Implement factorial function using recursion
    Added a recursive implementation for calculating factorial.

--- Number Theory/Factorial.java
+++ Number Theory/Factorial.java
@@ -9,3 +9,20 @@ int factorial(int n) {
     return ans;
 }
 
+
+//Optimise Code by Recursion
+
+int factorial(int n) {
+    // code here
+    
+    if(n == 0 || n == 1){
+        return 1;
+    }
+    
+    
+    int ans = n*factorial(n-1);
+    
+    return ans;
+    
+    
+}
