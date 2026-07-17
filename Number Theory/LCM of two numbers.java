ff7b6c942587fdc8a609e4274580cb76a1264645
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-17 06:06:52 +0530

    Implement LCM calculation in Solution class

--- Number Theory/LCM of two numbers.java
+++ Number Theory/LCM of two numbers.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public int lcm(int a, int b) {
+        // code here
+        int ans = 0;
+        int ans1 = 0;
+        int res = 0;
+        int min = 0;
+        int c = a* b;
+        
+        ans = Math.min(a , b);
+        ans1 = Math.max(a, b);
+        
+        while(ans1 >= ans && ans != 0){
+            min = ans;
+            ans = ans1 % ans;
+            ans1 = min;
+        }
+        
+        res = c / ans1;
+        
+        return res;
+      
+    }
+}
