7fba36f81bfee0170895440bf9df859d45cc98bc
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-17 06:08:50 +0530

    Add largestPrimeFactor method in Solution class
    Implement method to find the largest prime factor of a number.

--- Number Theory/Largest Prime Factor of a number .java
+++ Number Theory/Largest Prime Factor of a number .java
@@ -1,1 +1,24 @@
-
+class Solution {
+    static int largestPrimeFactor(int n) {
+        // code here
+        
+        int ans1 = 0;
+        while(n % 2 == 0){
+            n = n / 2;
+            ans1 = 2;
+        }
+        
+        for(int i = 3; i*i<=n; i+=2){
+            
+            while(n % i == 0){
+                n = n / i;
+                ans1 = i;
+            }
+        }
+        
+        if(n > 2){
+            ans1 = n;
+        }
+        return ans1;
+    }
+}
