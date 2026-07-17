6a7e8601f1f74595265a2d71f36c20bc21108cc8
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-18 01:40:28 +0530

    Add floorSqrt method to calculate square root
    Implement brute force method to calculate floor of square root.

--- Number Theory/Square Root.java
+++ Number Theory/Square Root.java
@@ -1,1 +1,14 @@
 //Brute force
+
+class Solution {
+    int floorSqrt(int n) {
+
+        int ans = 0;
+
+        for (int i = 1; i * i <= n; i++) {
+            ans = i;
+        }
+
+        return ans;
+    }
+}
