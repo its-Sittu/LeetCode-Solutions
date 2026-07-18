63f47acdce72f317de2b800b7fc1e86d1252e9b8
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-18 11:56:31 +0530

    Add trailingZeroes method to calculate trailing zeroes
    Implement method to calculate trailing zeroes in factorial.

--- Number Theory/trailing Zero.java
+++ Number Theory/trailing Zero.java
@@ -1,1 +1,34 @@
-
+class Solution {
+    public static int trailingZeroes(int n) {
+        // code here
+        
+        if( n == 0 || n == 1){
+            return 0;
+        }
+        
+        long ans = 1;
+        int count = 0;
+        
+        
+        int temp = n;
+        
+        while(temp != 1){
+            ans = ans*temp;
+            temp--;
+        }
+        
+      while(ans > 0){
+          
+         long digit = ans % 10;
+         
+         if(digit == 0){
+             count++;
+         } else{
+             break;
+         }
+         ans = ans/10;
+         
+      }
+      return count;
+    }
+}
