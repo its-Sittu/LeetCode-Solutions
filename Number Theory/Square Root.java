526d5d41885a5f476697d8eb88989fc107743286
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-18 11:09:25 +0530

    Add binary search method for floor square root

--- Number Theory/Square Root.java
+++ Number Theory/Square Root.java
@@ -22,3 +22,31 @@ class Solution {
         return (int)Math.sqrt(n);
     }
 }
+
+// Binary Search
+
+class Solution {
+    int floorSqrt(int n) {
+        // code here
+        int low = 1;
+        int high = n;
+        int ans = 0;
+        
+        while(low <= high ){
+            
+            int mid = low + (high - low)/2;
+            
+            long square = 1L*mid*mid;
+            
+            if(square == n){
+                return mid;
+            } else if(square <n){
+                ans = mid;
+                low = mid+1;
+            }else{
+                high = mid -1;
+            }
+        }
+        return ans;
+    }
+}
