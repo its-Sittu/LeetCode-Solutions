615a899229587131b4fdfc6b87491fba171f65de
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-07-24 16:23:56 +0530

    Add singleNumber method to find unique number
    Implement singleNumber method to find the unique number in an array using a HashMap.

--- HashMap_HashSet/Single number.java
+++ HashMap_HashSet/Single number.java
@@ -1,1 +1,25 @@
 // hashmap
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        int n = nums.length;
+      
+      for(int num : nums){
+        if(!map.containsKey(num)){
+            map.put(num , 0);
+        } else{
+            map.put(num, map.get(num)+1);
+        }
+      }
+
+      for(int num : nums){
+        if(map.get(num) != 1){
+            return num;
+        }
+      }
+      return -1;
+    }
+}
