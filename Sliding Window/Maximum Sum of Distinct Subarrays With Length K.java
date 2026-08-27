cc43536ce046926d44f41c516cfc069c8f5143ce
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-08-27 15:28:06 +0530

    Implement maximum sum of distinct subarrays of length k

--- Sliding Window/Maximum Sum of Distinct Subarrays With Length K.java
+++ Sliding Window/Maximum Sum of Distinct Subarrays With Length K.java
@@ -1,1 +1,31 @@
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        long max = 0;
+
+        for (int i = 0; i <= n - k; i++) {
+            HashSet<Integer> set = new HashSet<>();
+            boolean isDup = false;
+
+            long sum = 0;
+
+            for (int j = i; j <= i + k - 1; j++) {
+                if (set.contains(nums[j])) {
+                    isDup = true;
+                    break;
+                }
+
+                set.add(nums[j]);
+                sum = sum + nums[j];
+            }
+
+            if (!isDup) {
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+}
+
+
 
