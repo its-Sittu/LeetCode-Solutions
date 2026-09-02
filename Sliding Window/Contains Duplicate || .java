c6e8c82d1e2bfb3a36fca406b85903899dc56262
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-09-02 23:51:37 +0530

    Add brute force method to check for duplicates
    Implement brute force solution to check for nearby duplicates in an array.

--- Sliding Window/Contains Duplicate || .java
+++ Sliding Window/Contains Duplicate || .java
@@ -1,1 +1,23 @@
+// Brute Code 
 
+
+
+  class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        boolean isDig = false;
+
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(nums[i] == nums[j]){
+                  if(Math.abs(i-j)<= k){
+                      isDig = true;
+                      break;
+                  }
+                }
+            }
+        }
+
+        return isDig ;
+    }
+}
