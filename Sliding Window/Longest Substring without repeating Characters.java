f4a2881d12e45bca015c8fded1b7035be2291c25
Sittu Kumar Singh <sitturaj730@gmail.com>
2026-09-14 01:05:37 +0530

    Implement lengthOfLongestSubstring method

--- Sliding Window/Longest Substring without repeating Characters.java
+++ Sliding Window/Longest Substring without repeating Characters.java
@@ -21,3 +21,34 @@ class Solution {
         return max;
     }
 }
+
+
+--------------------------------------------------------------------------
+
+
+
+
+    class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+
+        int max = 0;
+        int i = 0, j = 0;
+        HashSet<Character> set = new HashSet<>();
+
+        while (j < n) {
+
+            char c = s.charAt(j);
+
+            while (set.contains(c)) {
+                set.remove(s.charAt(i));
+                i++;
+            }
+
+            set.add(c);
+            max = Math.max(max, j - i + 1);
+            j++;
+        }
+        return max;
+    }
+}
